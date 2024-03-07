7754ad878ac50518d7f9b5ad781ed72b5ba67182
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-07 01:08:05 +0000

    Usando foreach, escreve programa que mostra no ecrâ os argumentos da linha de comandos com comprimento superior a 3 caracteres

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (arg.Length > 6)
+                {
+                    // Se um argumento tiver mais de 6 caracteres, termina o programa
+                    return;
+                }
+                else if (args.Length > 3)
+                {
+                    // Mostra o argumento se tiver mais de 3 caracteres
+                    Console.WriteLine(arg);
+                }            
+            }
+        }
+    }
+}
